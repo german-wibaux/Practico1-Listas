dd57bbe08318e55bc1594a6377a50b77db7d8d6c
german <germanwibaux@gmail.com>
2018-04-10 23:42:41 -0300

    Second part complete

--- src/defaultpackage/Lists.java
+++ src/defaultpackage/Lists.java
@@ -2,6 +2,7 @@ package defaultpackage;
 
 import defaultpackage.MySimpleLinkedList.MyIterator;
 
+
 public class Lists {
 	
 	public static void listElementsRepeteadInOrder( MySimpleLinkedList list1 , MySimpleLinkedList list2 , MySimpleLinkedList  listResult) {
@@ -20,10 +21,8 @@ public class Lists {
 				
 				if (elemCompare <= elemCompare2) {
 					ctrl=true;
-					if ( elemCompare == elemCompare2 ) {
-						System.out.print(elemCompare + "  " +  elemCompare2 + "\n");
-						listResult.insertLastPosition(elemCompare);
-					}						
+					if ( elemCompare == elemCompare2 ) 
+						listResult.insertLastPosition(elemCompare);											
 					iter2.resetPointer();
 				}
 				
@@ -32,34 +31,86 @@ public class Lists {
 			iter2.resetPointer();
 			
 		}
-		
 	}
+		
+		public static void listElementsRepeteadNotOrder(MySimpleLinkedList list1 , MySimpleLinkedList list2 , MySimpleLinkedList  listResult) {
+			
+			MyIterator iter1 = list1.iterator();
+			MyIterator iter2 = list2.iterator();
+		
+			while(iter1.hasNext()) {
+				
+				boolean ctrl = false;
+				int elemCompare = (int) iter1.next().getInfo();
+				
+				while((iter2.hasNext())&& (!ctrl)) {
+					
+					int elemCompare2 = (int) iter2.next().getInfo();
+					
+					if (elemCompare == elemCompare2) {
+						System.out.println(elemCompare);
+						ctrl=true;
+						listResult.insertInOrder(elemCompare);
+					}
+				}
+				
+				iter2.resetPointer();
+			}
+		}
+		
+	
 
 	public static void main(String[] args) {
 	
 		MySimpleLinkedList list1 = new MySimpleLinkedList();
 		MySimpleLinkedList list2 = new MySimpleLinkedList();
-		MySimpleLinkedList listResult = new MySimpleLinkedList();
+		MySimpleLinkedList listResult = new MySimpleLinkedList();		
 		
-		list1.insertInOrder(20);
+		System.out.println("////////////////////Lists in order//////////////////////////");
+		
 		list1.insertInOrder(10);
-		list1.insertInOrder(30);
-		list1.insertInOrder(25);
 		list1.insertInOrder(15);
+		list1.insertInOrder(20);
+		list1.insertInOrder(25);
+		list1.insertInOrder(30);		
+		list1.insertInOrder(100);
 		
-		list2.insertInOrder(20);
-		list2.insertInOrder(75);
-		list2.insertInOrder(70);
-		list2.insertInOrder(25);
 		list2.insertInOrder(15);
-		list2.insertInOrder(100);
+		list2.insertInOrder(20);
+		list2.insertInOrder(25);
+		list2.insertInOrder(70);
+		list2.insertInOrder(75);
+		list2.insertInOrder(100);	
 		
-		listElementsRepeteadInOrder(list1 , list2 , listResult);
-		
+		listElementsRepeteadInOrder(list1 , list2 , listResult);	
 		
 		listResult.showList();
 		
+		System.out.println("//////////////////////lists without order////////////////////////");
 		
+		MySimpleLinkedList list_1 = new MySimpleLinkedList();
+		MySimpleLinkedList list_2 = new MySimpleLinkedList();
+		MySimpleLinkedList list_Result = new MySimpleLinkedList();
+		
+		list_1.insert(6);
+		list_1.insert(5);
+		list_1.insert(3);
+		list_1.insert(8);
+		list_1.insert(25);		
+		list_1.insert(10);
+		
+		list_2.insert(2);
+		list_2.insert(99);
+		list_2.insert(11);
+		list_2.insert(8);
+		list_2.insert(6);
+		list_2.insert(5);
+		list_2.insertInOrder(1);
+		list_2.insertInOrder(100);
+		
+		listElementsRepeteadNotOrder(list_1 , list_2 , list_Result);
+		
+		list_Result.showList();
 		
 	}
 
